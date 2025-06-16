b7b06eab861406452622628d5e304be51dbc3fcc
Choi Yeonung <yonsweng@gmail.com>
2025-06-16 15:40:22 +0900

    refactor: simplify next.config.js by removing unnecessary config loading logic

--- next.config.js
+++ next.config.js
@@ -2,38 +2,21 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
-const { join } = require('path');
-const { readFileSync } = require('fs');
 
-let config;
-try {
-  const configPath = join(__dirname, 'next.config.original');
-  const originalConfig = readFileSync(configPath, 'utf8');
-  // eslint-disable-next-line no-eval
-  config = eval(originalConfig.replace('module.exports =', ''));
-} catch {
-  config = {
-    eslint: {
-      dirs: ['.'],
-    },
-    poweredByHeader: false,
-    trailingSlash: true,
-    basePath: '',
-    reactStrictMode: true,
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-  };
-}
-
-const appHostingConfig = {
-  ...config,
-  ...(config.images?.unoptimized === undefined && {
-    images: { unoptimized: true },
-  }),
+module.exports = withBundleAnalyzer({
+  eslint: {
+    dirs: ['.'],
+  },
+  poweredByHeader: false,
   trailingSlash: true,
-  output: undefined,
-};
-
-module.exports = withBundleAnalyzer(appHostingConfig);
+  basePath: '',
+  // The starter code load resources from `public` folder with `router.basePath` in React components.
+  // So, the source code is "basePath-ready".
+  // You can remove `basePath` if you don't need it.
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  ignoreDuringBuilds: true,
+});
