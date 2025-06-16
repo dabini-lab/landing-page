e486acffc747b6152f461ea6a97f76ab8fc149dd
Choi Yeonung <yonsweng@gmail.com>
2025-06-16 12:34:55 +0900

    feat: implement user authentication in Base and Hero components

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -1,6 +1,14 @@
-import Link from 'next/link';
+'use client';
 
-import { signInWithGoogle } from '@/lib/firebase/auth';
+import { deleteCookie, setCookie } from 'cookies-next';
+import Link from 'next/link';
+import { useEffect } from 'react';
+
+import {
+  onIdTokenChanged,
+  signInWithGoogle,
+  signOut,
+} from '@/lib/firebase/auth';
 
 import { Background } from '../background/Background';
 import { Button, ButtonColor } from '../button/Button';
@@ -9,46 +17,82 @@ import { Section } from '../layout/Section';
 import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 import { Logo } from './Logo';
 
+const useUserSession = (initialUser: object | any) => {
+  useEffect(() => {
+    return onIdTokenChanged(async (user) => {
+      if (user) {
+        const idToken = await user.getIdToken();
+        await setCookie('__session', idToken);
+      } else {
+        await deleteCookie('__session');
+      }
+      if (initialUser?.uid === user?.uid) {
+        return;
+      }
+      window.location.reload();
+    });
+  }, [initialUser]);
+
+  return initialUser;
+};
+
 const handleSignIn = (event: React.MouseEvent<HTMLAnchorElement>) => {
   event.preventDefault();
-  console.log('Sign in clicked');
   signInWithGoogle();
 };
 
-const Hero = () => (
-  <Background color="bg-gray-100">
-    <Section yPadding="py-6">
-      <NavbarTwoColumns logo={<Logo xl />}>
-        <li>
-          {/* <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            GitHub
-          </Link> */}
-        </li>
-        <li>
-          <Link href="/" onClick={handleSignIn}>
-            Sign in
-          </Link>
-        </li>
-      </NavbarTwoColumns>
-    </Section>
+const handleSignOut = (event: React.MouseEvent<HTMLAnchorElement>) => {
+  event.preventDefault();
+  deleteCookie('__session');
+  signOut();
+};
 
-    <Section yPadding="pt-20 pb-32">
-      <HeroOneButton
-        title={
-          <>
-            {'An AI chatbot that\n'}
-            <span className="text-primary-500">you have always desired</span>
-          </>
-        }
-        description="The easiest way to ask something to the AI."
-        button={
-          <Button xl color={ButtonColor.DISCORD}>
-            Add to Discord
-          </Button>
-        }
-      />
-    </Section>
-  </Background>
-);
+const Hero = ({ initialUser }: { initialUser: any }) => {
+  const user = useUserSession(initialUser);
+
+  return (
+    <Background color="bg-gray-100">
+      <Section yPadding="py-6">
+        <NavbarTwoColumns logo={<Logo xl />}>
+          <li>
+            {/* <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
+              GitHub
+            </Link> */}
+          </li>
+          {user ? (
+            <li>
+              <Link href="/" onClick={handleSignOut}>
+                Sign out
+              </Link>
+            </li>
+          ) : (
+            <li>
+              <Link href="/" onClick={handleSignIn}>
+                Sign in
+              </Link>
+            </li>
+          )}
+        </NavbarTwoColumns>
+      </Section>
+
+      <Section yPadding="pt-20 pb-32">
+        <HeroOneButton
+          title={
+            <>
+              {'An AI chatbot that\n'}
+              <span className="text-primary-500">you have always desired</span>
+            </>
+          }
+          description="The easiest way to ask something to the AI."
+          button={
+            <Button xl color={ButtonColor.DISCORD}>
+              Add to Discord
+            </Button>
+          }
+        />
+      </Section>
+    </Background>
+  );
+};
 
 export { Hero };
